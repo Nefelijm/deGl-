fc75d70c4871ecd40962761246a09e84ca397186
MariaRuizQ <ruiz.maria.quispe@gmail.com>
2018-01-08 17:16:44 -0500

    Añadiendo funcionalidad

--- js/homepage.js
+++ js/homepage.js
@@ -1,37 +1,41 @@
 $(document).ready(function() {
   var $btnPhoto = $('.btn-photo');
   var $btnReceta = $('.btn-recipe');
-  var $toPost = $('.to-post');
+  var $BtnToPost = $('.btn-to-post');
+  var $textarea = $('.textarea');
+  var $imgPost = $('.img-post');
+  var $nameRecipe = $('.name-recipe')
 
+  //Función para llamar y ocultar form para postear imágenes/videos
   $btnPhoto.on('click',function(e){
     var $modal = $('.photo');
-    //$modal.append("<form><div class="form-group"><label for="exampleInputEmail1">Nombre</label><input type="email" class="form-control" id="exampleInputEmail1" placeholder="Escribe el nombre de la imágen"></div><div class="form-group"><label for="exampleInputFile">File input</label><input type="file" id="exampleInputFile"></div></form>");
     $('.recipe').hide();
     $('.photo').show();
   });
 
+  //Función para llamar y ocultar form para postear recetas
   $btnReceta.on('click',function(e){
     var $modal2 = $('.recipe');
-    //$modal2.append("<form><div class="form-group"><label for="exampleInputEmail1">Nombre</label><input type="email" class="form-control" id="exampleInputEmail1" placeholder="Escribe el nombre de la imágen"></div><div class="form-group"><label for="exampleInputFile">File input</label><input type="file" id="exampleInputFile"></div></form>");
     $('.photo').hide();
     $('.recipe').show();
   });
-});
 
-
-$textarea.on('keyup',function(e){
-    console.log(e.target);
-    var $val =$(this).val();
-    console.log($(this).val().length);
-    var $veinte = $(this).val().length;
-     if ($veinte > 20) {
-       $(this).css( "color", "red" );
-       $('#tweet-btn').removeAttr('disabled');
-     };
+  //Función para agregar color al textarea y habilitar botón
+  $textarea.on('keyup',function(e){
+    var $val = $(this).val();
+    var $text = $(this).val().length;
+     if ($text > 0){
+       $(this).css("color", "pink");
+       $('.btn-to-post').removeAttr('disabled');
+     }
   });
 
-  var $val =$(this).val();
-  $button.on('click',function(e){
+  //Función para que el contenido escrito en el modal se agregue en la sección de notificaciones
+  $BtnToPost.on('click',function(e){
     event.preventDefault();
-    $('#messages').prepend($textarea.val());
+    $('.add-post-name').prepend($nameRecipe.val());
+    $('.add-post-img').prepend($imgPost.val());
+    $('.add-post-content').prepend($textarea.val());
+    $('.modal-post').hide();
   });
+});
