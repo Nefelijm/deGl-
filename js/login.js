a140e216e215d08e449cc5086a48473ef23b5d81
Nefelijm <sistemasnik20@gmail.com>
2018-01-10 17:13:33 -0500

    Terminando de validar el login

--- js/login.js
+++ js/login.js
@@ -10,13 +10,63 @@ $(document).ready(function() {
   };
   firebase.initializeApp(config);
 
-  // Obteniendo todo los elementos del DOM
 
+  // Obteniendo todo los elementos del DOM
   var email = $('#textEmail');
   var password = $('#textPassword');
   var login = $('#btnLogin');
   var sign = $('#btnSign');
   var logout = $('#btnLogout');
+
+  // Creando Variables que nos ayudaran 
+  var verifyEmail = false;
+  var verifyPassword = false;
+
+  // Creando Funciones para que se active el boton
+  function activeButton() {
+    if (verifyEmail === true && verifyPassword === true) {
+      $('#btnLogin').attr('disabled', false);
+      $('#btnSign').attr('disabled', false);
+    }
+  }
+
+  function desactiveButton() {
+    if (verifyEmail === true && verifyPassword === true) {
+      $('#btnLogin').attr('disabled', true);
+      $('#btnSign').attr('disabled', true);
+    }
+  }    
+
+  // Validando correo
+
+  $('#textEmail').on('input', function() {
+    var parameterEmail = new RegExp(/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/); // Expresiones regulares que nos ayudara a validar el email
+    if (parameterEmail.test($(this).val()) === false || $(this).val() === '') {
+      $('#msj1').text('Ingrese su correo correcto');
+      $(this).focus();
+      desactiveButton();
+      return false;
+    } else {
+      $('#msj1').text('');
+      verifyEmail = true;
+      activeButton();
+    }
+  });
+
+  // Validando Password
+
+  $('#textPassword').on('input', function(event) {
+    if ($(this).val().length >= 6) {      
+      $('#msj2').text('Ingrese su Password correcto');
+      $(this).focus();
+      desactiveButton();
+      return false;
+    } else {
+      $('#msj2').text('');
+      verifyPassword = true;
+      activeButton();
+    }
+  });
 
   // Evento click para obtener los valores de los inputs
 
@@ -30,29 +80,25 @@ $(document).ready(function() {
     var promise = auth.signInWithEmailAndPassword(emailLogin, passwordLogin);
     promise.catch(e => console.log(e.message));
     window.location.href = '../views/homepage.html';
-<<<<<<< HEAD
-=======
-
->>>>>>> c02ec87ead3cb4515d701df7f9376dfcfe673fe7
   });
+
+  $('#btnSign').on('click', e => {
     e.preventDefault();
     var emailLogin = email.val();
     var passwordLogin = password.val();
     var auth = firebase.auth();
-    // Hacemos el metodo de identficacion
+    // Hacemos el metodo de identficacion 
     var promise = auth.createUserWithEmailAndPassword(emailLogin, passwordLogin);
     promise.catch(e => console.log(e.message));
     window.location.href = '../views/homepage.html';
   });
 
   $('#btnLogout').on('click', e => {
-      e.preventDefault();
+    e.preventDefault();
     firebase.auth().signOut();
-      $(location).attr('href', 'photo.html');
   });
 
   firebase.auth().onAuthStateChanged(firebaseUser => {
-
     if (firebaseUser) {
       console.log(firebaseUser);
       btnLogout.classList.remove('hide');
