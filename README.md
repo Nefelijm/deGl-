07245cba5931880e243605c8bac794187ad64096
Nefelijm <sistemasnik20@gmail.com>
2018-01-10 08:50:17 -0500

    arreglando login

--- README.md
+++ README.md
@@ -10,5 +10,6 @@
 
 1.Se muestra la vista de carga por cinco segundo
 
-[Recursos para la AGENDA](https://www.canva.com/design/DACsEGjMckk/y4nRnK1IkqY--aqZvvX0lw/view?website "PLANIFICACION").
+[Recursos para la AGENDA
+](https://www.canva.com/design/DACsEGjMckk/y4nRnK1IkqY--aqZvvX0lw/view?website "PLANIFICACION").
 
--- js/login.js
+++ js/login.js
@@ -1,78 +1,62 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  // Initialize Firebase
+  var config = {
+    apiKey: 'AIzaSyAlbcybJZDOmr_rLwWDzAnarW7U23C8ZyI',
+    authDomain: 'deglu-producto.firebaseapp.com',
+    databaseURL: 'https://deglu-producto.firebaseio.com',
+    projectId: 'deglu-producto',
+    storageBucket: '',
+    messagingSenderId: '926786399247'
+  };
+  firebase.initializeApp(config);
 
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyAlbcybJZDOmr_rLwWDzAnarW7U23C8ZyI",
-        authDomain: "deglu-producto.firebaseapp.com",
-        databaseURL: "https://deglu-producto.firebaseio.com",
-        projectId: "deglu-producto",
-        storageBucket: "",
-        messagingSenderId: "926786399247"
-    };
-    firebase.initializeApp(config);
+  // Obteniendo todo los elementos del DOM
 
-//Obteniendo todo los elementos del DOM
+  var email = $('#textEmail');
+  var password = $('#textPassword');
+  var login = $('#btnLogin');
+  var sign = $('#btnSign');
+  var logout = $('#btnLogout');
 
-    var email = $('#textEmail');
-    var password = $('#textPassword');
-    var login = $('#btnLogin');
-    var sign = $('#btnSign');
-    var logout = $('#btnLogout');
+  // Evento click para obtener los valores de los inputs
 
-//Evento click para obtener los valores de los inputs
+  $('#btnLogin').on('click', e => {
+    e.preventDefault();
+    var emailLogin = email.val();
+    console.log(emailLogin);
+    var passwordLogin = password.val();
+    var auth = firebase.auth();
+    // Hacemos el metodo de identficacion 
+    var promise = auth.signInWithEmailAndPassword(emailLogin, passwordLogin);
+    promise.catch(e => console.log(e.message));
+      
+  });
 
-    $('#btnLogin').on('click',function(e){
-    var email = $('#textEmail').val();
-    var password = $('#textPassword').val();
-    //Hacemos el metodo de identficacion 
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error){
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        if (errorCode) {
-            $('#textPassword').val('');
-            alert(errorMessage);
-        }
-    });   
+  // Evento click para registrar usuario
+  $('#btnSign').on('click', e => {
+    e.preventDefault();
+    var emailLogin = email.val();
+    var passwordLogin = password.val();
+    var auth = firebase.auth();
+    // Hacemos el metodo de identficacion 
+    var promise = auth.createUserWithEmailAndPassword(emailLogin, passwordLogin);
+    promise.catch(e => console.log(e.message));
+  });
+
+  $('#btnLogout').on('click', e => {
+      e.preventDefault();
+    firebase.auth().signOut();
+      $(location).attr('href', 'home.html');
+  });
+
+  firebase.auth().onAuthStateChanged(firebaseUser => {
     
-});
-
-//Evento click para registrar usuario
-    $('#btnSign').on('click',function(e){
-        e.preventDefault();
-    var email = $('#textEmail').val();
-       console.log(email);
-    var password = $('#textPassword').val();
-      console.log(password);
-    //Hacemos el metodo de identficacion 
-        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-  
-     });
-    }); 
-    
-        firebase.auth().onAuthStateChanged(function (user) {
-            if (user) {
-                // User is signed in.
-                var email = user.email;
-                var emailVerified = user.emailVerified;
-                console.log(email);
-                // window.location.href = '../views/feed.html';
-            } else {
-                console.log('usuario no logeado');
-            }
-        });
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (firebaseUser) {
+      console.log(firebaseUser);
+      btnLogout.classList.remove('hide');
+    } else {
+      console.log('no logueado');
+      btnLogout.classList.add('hide');
+    }
+  });
 });
