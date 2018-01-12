7a41e71f6b4a8eb02dbcda71fbeaed46573f50d9
MariaRuizQ <ruiz.maria.quispe@gmail.com>
2018-01-12 14:30:49 -0500

    Añadiendo últimos

--- js/redireccionando.js
+++ js/redireccionando.js
@@ -4,11 +4,16 @@ $(document).ready(function() {
   var $logo = $('.logo');
   var $behind = $('.behind');
   var $behind2 = $('.behind2');
-  var $iconProlife = $('.icon-prolife')
+  var $iconProlife = $('.icon-prolife');
+  var $btnReceta = $('.btn-receta');
 
   //Evento al btn información, que me redirecciona al form para agregar tus datos
   $btnInformation.on('click',function(e){
     $(location).attr('href', 'personalInformation.html');
+  });
+  //Evento al bnt recetas, que me redirecciona a la vista de recetas
+  $btnReceta.on('click',function(e){
+    $(location).attr('href', 'recetas.html');
   });
   //Evento al logo y atrás del prolife y del personalInformation, que me redirecciona a la vista homepage
   $logo.on('click',function(e){
